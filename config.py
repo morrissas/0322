ea86ebfc17fe81b1b6e69a4ab6a4f1bf4fa1900f
morrissas <morris861003@gmail.com>
2024-04-08 11:53:58 +0800

    local db

---
+++ config.py
@@ -0,0 +1,4 @@
+PineconeKey = "03c35daf-b2c6-457e-9d3a-2fc9be5d972b"
+OpenAIKey = "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"
+IndexName = "morris"
+localize = False #True Chroma database(localize) ,False use Pinecone database
